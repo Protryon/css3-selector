4b862ee1efc501573d9fe80f8468bbc3e9e519e0
Protryon <max.bruce12@gmail.com>
2020-05-20 06:28:39 -0700

    serde + v0.1.1

--- src/ast.rs
+++ src/ast.rs
@@ -1,20 +1,27 @@
 /// https://www.w3.org/TR/2018/REC-selectors-3-20181106/#grammar
-
+use serde::{Deserialize, Serialize};
 pub trait Node<'a> {}
 
+#[derive(Deserialize, Serialize, Clone)]
 pub struct SelectorsGroup<'a> {
+    #[serde(borrow)]
     pub selectors: Vec<Selector<'a>>,
 }
 
 impl<'a> Node<'a> for SelectorsGroup<'a> {}
 
+#[derive(Deserialize, Serialize, Clone)]
 pub struct Selector<'a> {
+    #[serde(borrow)]
     pub base: SelectorSequence<'a>,
+    #[serde(borrow)]
     pub modifiers: Vec<(Combinator, SelectorSequence<'a>)>,
 }
 
 impl<'a> Node<'a> for Selector<'a> {}
 
+#[derive(Deserialize, Serialize, Clone)]
+#[serde(tag = "type")]
 pub enum Combinator {
     Plus,
     Greater,
@@ -22,13 +29,18 @@ pub enum Combinator {
     None,
 }
 
+#[derive(Deserialize, Serialize, Clone)]
 pub struct SelectorSequence<'a> {
+    #[serde(borrow)]
     pub type_selector: Option<TypeSelector<'a>>,
+    #[serde(borrow)]
     pub attribute_selectors: Vec<AttributeSelector<'a>>,
 }
 
 impl<'a> Node<'a> for SelectorSequence<'a> {}
 
+#[derive(Deserialize, Serialize, Clone)]
+#[serde(tag = "type")]
 pub enum AttributeSelector<'a> {
     Hash(&'a str),
     Class(&'a str),
@@ -39,6 +51,8 @@ pub enum AttributeSelector<'a> {
 
 impl<'a> Node<'a> for AttributeSelector<'a> {}
 
+#[derive(Deserialize, Serialize, Clone)]
+#[serde(tag = "type")]
 pub enum Namespace<'a> {
     All,
     Ident(&'a str),
@@ -46,6 +60,7 @@ pub enum Namespace<'a> {
 
 impl<'a> Node<'a> for Namespace<'a> {}
 
+#[derive(Deserialize, Serialize, Clone)]
 pub struct TypeSelector<'a> {
     pub namespace: Option<Namespace<'a>>,
     pub element_name: Option<&'a str>,
@@ -53,6 +68,7 @@ pub struct TypeSelector<'a> {
 
 impl<'a> Node<'a> for TypeSelector<'a> {}
 
+#[derive(Deserialize, Serialize, Clone)]
 pub enum Matcher {
     Prefix,
     Suffix,
@@ -62,6 +78,7 @@ pub enum Matcher {
     Dash,
 }
 
+#[derive(Deserialize, Serialize, Clone)]
 pub struct Attribute<'a> {
     pub namespace: Option<Namespace<'a>>,
     pub name: &'a str,
@@ -71,6 +88,7 @@ pub struct Attribute<'a> {
 
 impl<'a> Node<'a> for Attribute<'a> {}
 
+#[derive(Deserialize, Serialize, Clone)]
 pub struct Psuedo<'a> {
     pub is_class_type: bool, // if true, only one colon was used
     pub name: &'a str,
@@ -79,12 +97,15 @@ pub struct Psuedo<'a> {
 
 impl<'a> Node<'a> for Psuedo<'a> {}
 
+#[derive(Deserialize, Serialize, Clone)]
 pub struct Expression<'a> {
+    #[serde(borrow)]
     pub items: Vec<ExpressionItem<'a>>,
 }
 
 impl<'a> Node<'a> for Expression<'a> {}
 
+#[derive(Deserialize, Serialize, Clone)]
 pub enum ExpressionItem<'a> {
     Plus,
     Minus,
@@ -96,6 +117,8 @@ pub enum ExpressionItem<'a> {
 
 impl<'a> Node<'a> for ExpressionItem<'a> {}
 
+#[derive(Deserialize, Serialize, Clone)]
+#[serde(tag = "type")]
 pub enum Negation<'a> {
     TypeSelector(TypeSelector<'a>),
     Hash(&'a str),
